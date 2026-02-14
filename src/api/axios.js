7fb93e2dc5930065bd4a64fbcc22eaf53858424e
maxpain62 <maxpain62@gmail.com>
2026-02-14 11:20:56 +0530

    changed baseurl to nodeip:nodeport to correct error "ERR_NAME_NOT_RESOLVED"

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://eos-gateway-api-service.eos-dev.svc.cluster.local:5555",
+  //baseURL: "http://eos-gateway-api-service.eos-dev.svc.cluster.local:5555",
+  baseURL: "http://13.126.66.193:30004",
 });
