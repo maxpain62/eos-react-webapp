2dc13a16dfedfdf7d6fa4b1130ca0402b7668522
maxpain62 <maxpain62@gmail.com>
2026-02-15 12:21:21 +0530

    update axios baseURL to new IP address

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://eos-gateway-api-service.eos-dev.svc.cluster.local:5555",
-  baseURL: "http://13.126.66.193:30004",
+  baseURL: "http://3.110.80.28:30004",
 });
